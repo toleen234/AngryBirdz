16770854e09b8abf69ebef02db491006fa71d5bf
toleen234 <78652839+toleen234@users.noreply.github.com>
2021-04-30 17:09:57 -0400

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,26 @@
+class slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB=pointB
+        this.shot = Constraint.create(options);
+        World.add(world, this.shot);
+    }
+    fly(){
+        this.shot.bodyA=null
+    }
+
+    display(){
+        if(this.shot.bodyA){
+        var pointA = this.shot.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
